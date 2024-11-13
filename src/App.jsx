557dfd4d95d278e50beb7284cff2291b56381279
Arda Halibryam <ardahalibrqqm@gmail.com>
2024-11-13 10:21:44 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React, { useRef, useState, useEffect } from "react";
 import Camera from './Camera.jsx';
 import Quiz from './Quiz.jsx';
+import Second from './Second.jsx';
 
 function App() {
 
   return(
-    <>
-    <Camera />
-    <Quiz />
-    </>
+    <Second />
   );
 }
 
