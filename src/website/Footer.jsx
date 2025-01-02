48ad07e125622da76aec59f86e1f2e39b0d40c08
Arda Halibryam <ardahalibrqqm@gmail.com>
2025-01-02 10:21:31 +0200

    Create Footer.jsx

---
+++ src/website/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './website.css';
+
+function Footer() {
+
+  return (
+    <footer>
+        <div className="footer-nav" id="footer-nav-1">
+            <a href="">link1 qf</a>
+            <a href="">link1 fq</a>
+            <a href="">link 41 ee</a>
+        </div>
+        <div className="footer-middle">
+            <img src="src\img\logo.png" alt="logo" />
+        </div>
+        <div className="footer-nav" id="footer-nav-2">
+            <a href="">link rqqer</a>
+            <a href="">link fa</a>
+            <a href="">link fafafa</a>
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
