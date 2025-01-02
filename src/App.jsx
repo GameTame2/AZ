3c4e5fd5dca36949abdb2d157421644e5f90e258
Arda Halibryam <ardahalibrqqm@gmail.com>
2025-01-02 10:20:36 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 import React, { useState } from 'react';
-import CameraQuestions from './CameraQuestions';
-import TestQuestions from './TestQuestions';
+import CameraQuestions from './app/CameraQuestions';
+import TestQuestions from './app/TestQuestions';
+import './app/App.css';
 
 function App() {
-  // State to track which component to display
-  const [showCameraQuestions, setShowCameraQuestions] = useState(true);
-
-  // Function to handle the "Next" button click
-  const handleNext = () => {
-    setShowCameraQuestions(false);  // Switch to TestQuestions
-  };
 
   return (
     <>
-      <button onClick={handleNext}>Next</button>
-      
-      {showCameraQuestions ? <CameraQuestions /> : <TestQuestions />}
+      <CameraQuestions/>
+      <TestQuestions/>
     </>
   );
 }
