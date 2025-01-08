669491e9664c975ab25901e95a3317a0404056ce
Arda Halibryam <ardahalibrqqm@gmail.com>
2025-01-08 10:42:22 +0200

    Create Cta.jsx

---
+++ src/components/website/Cta.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/website.css';
+
+function Cta() {
+  return (
+    <div className="cta">
+        <img src="src/img/cta.png" alt="cta-button" />
+    </div>
+  );
+}
+
+export default Cta;
