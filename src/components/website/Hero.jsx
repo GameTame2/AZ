fa576d699dc6e4099f8a2e5441f642bd6d234765
Arda Halibryam <ardahalibrqqm@gmail.com>
2025-01-08 10:42:39 +0200

    Create Hero.jsx

---
+++ src/components/website/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../../styles/website.css';
+
+function Hero() {
+
+  return (
+    <div className="hero">
+      <div className="hero-left">
+        <img src="src\img\arrow-left.png" alt="arrow-left" />
+      </div>
+      <div className="hero-middle">
+        <h1>АЗ.</h1>
+        <h2>“Да знаеш кой си е началото на всичко, което ще постигнеш.” - Ралф Емерсън</h2>
+      </div>
+      <div className="hero-right">
+        <img src="src\img\arrow-right.png"/>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
