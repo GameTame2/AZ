6883cdd70146d52e1038e88904a85b383f9bf0a8
Arda Halibryam <ardahalibrqqm@gmail.com>
2025-01-08 10:42:55 +0200

    Create Header.jsx

---
+++ src/components/website/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../../styles/website.css';
+
+function Header() {
+
+  return (
+    <>
+    <header> 
+        <div className="left-header">
+            <img src="src\img\logo.png" alt="" />
+        </div>
+        <div className="right-header">
+            <ul className="right-header-ul">
+                <li><a href="#">Начало.</a></li>
+                <li><a href="#">Защо?</a></li>
+                <li><a href="#">Тестове!</a></li>
+            </ul>
+        </div>
+    </header>
+    </>
+  );
+}
+
+export default Header;
