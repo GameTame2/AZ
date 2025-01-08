6f8a5116d8a6676e3feac22f9b1b6ae2c89a5e1a
Arda Halibryam <ardahalibrqqm@gmail.com>
2025-01-08 10:42:49 +0200

    Create Face.jsx

---
+++ src/components/website/Face.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import '../../styles/website.css';
+
+function Face() {
+
+  return (
+      <div className="face">
+
+      <div id="large-face-box">
+        <img src="src\img\face.png" alt="face image" />
+      </div>
+
+      <div className="face-box" id="face-box-1">
+        <img className="face-box" src="src\img\international-day.png" alt="icon" />
+        <p className="face-box-title">Наклонности</p>
+      </div>
+
+      <div className="face-box" id="face-box-2">
+        <img className="face-box" src="src\img\international-day.png" alt="icon" />
+        <p className="face-box-title">Наклонности</p>
+      </div>
+
+      <div className="face-box" id="face-box-3">
+        <img className="face-box" src="src\img\international-day.png" alt="icon" />
+        <p className="face-box-title">Наклонности</p>
+      </div>
+
+      <div className="face-box" id="face-box-4">
+        <img className="face-box" src="src\img\international-day.png" alt="icon" />
+        <p className="face-box-title">Наклонности</p>
+      </div>
+
+      <div className="face-box" id="face-box-5">
+        <img className="face-box" src="src\img\international-day.png" alt="icon" />
+        <p className="face-box-title">Наклонности</p>
+      </div>
+
+      <div className="face-box" id="face-box-6">
+        <img className="face-box" src="src\img\international-day.png" alt="icon" />
+        <p className="face-box-title">Наклонности</p>
+      </div>
+
+      </div>
+    
+  );
+}
+
+export default Face;
