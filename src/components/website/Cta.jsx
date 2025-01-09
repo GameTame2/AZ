6f064bd8b3dc0ba0e1c5675f9120e35e5ef3e8f1
Arda Halibryam <ardahalibrqqm@gmail.com>
2025-01-09 21:00:55 +0200

    Update Cta.jsx

--- src/components/website/Cta.jsx
+++ src/components/website/Cta.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import '../../styles/website.css';
 
 function Cta() {
   return (
     <div className="cta">
+      <Link to="/quiz">
         <img src="src/img/cta.png" alt="cta-button" />
+      </Link>
     </div>
   );
 }
