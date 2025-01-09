5efeaaa4f6407b268b747d73b227217c647fb293
Arda Halibryam <ardahalibrqqm@gmail.com>
2025-01-09 22:47:44 +0200

    Update Quiz.jsx

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import CameraQuestions from '../components/quiz/CameraQuestions.jsx';
 import TestQuestions from '../components/quiz/TestQuestions.jsx';
+import Test from '../components/quiz/Test.jsx';
 import '../styles/quiz.css';
 
 function Quiz() {
 
   return (
     <>
+      <Test />
       <CameraQuestions/>
       <TestQuestions/>
     </>
