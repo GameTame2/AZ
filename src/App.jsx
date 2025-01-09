808dcbc8cbe2546108025f2b74f2d512955aa991
Arda Halibryam <ardahalibrqqm@gmail.com>
2025-01-09 21:00:59 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { React, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css';
 import Website from './pages/Website.jsx';
 import Quiz from './pages/Quiz.jsx';
 
