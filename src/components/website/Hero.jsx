023f6aa78edea61d14c1e052f3f00ed66eeac85b
Arda Halibryam <ardahalibrqqm@gmail.com>
2025-01-30 21:11:13 +0200

    Update Hero.jsx

--- src/components/website/Hero.jsx
+++ src/components/website/Hero.jsx
@@ -10,7 +10,7 @@ function Hero() {
       </div>
       <div className="hero-middle">
         <div className="hero-title">
-          <img src="src\img\crown.png" alt="crown" />
+          <img src="src\img\crown_no_gems.png" alt="crown" />
           <h1>АЗ</h1>
         </div>
         <h2>“Да знаеш кой си е началото на всичко, което ще постигнеш.” - Ралф Емерсън</h2>
