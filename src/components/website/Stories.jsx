4b359d03f4559460b4b8b53e6cc2673235551f1d
Arda Halibryam <ardahalibrqqm@gmail.com>
2025-01-30 21:11:01 +0200

    Create Stories.jsx

---
+++ src/components/website/Stories.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../styles/website.css';
+
+function Stories() {
+
+  return (
+    <section className='stories'>
+        <h2>ИСТОРИИ</h2>
+        <div className="stories-img">
+            <img src="../../img/artist_woman.png" alt="character 1" />
+            <img src="" alt="character 2" />
+        </div>
+    </section>
+  );
+}
+
+export default Stories;
