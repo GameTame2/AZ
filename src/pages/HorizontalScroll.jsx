a6d4f29a33bd3607f2cdbfb7d97a8366e36f53bb
Arda Halibryam <ardahalibrqqm@gmail.com>
2025-01-30 21:09:53 +0200

    Create HorizontalScroll.jsx

---
+++ src/pages/HorizontalScroll.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { gsap } from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import Lenis from "@studio-freight/lenis";
+import { useEffect, useRef } from "react";
+import '../styles/horizontal.css';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const HorizontalScroll = () => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      smooth: true,
+      direction: "horizontal",
+    });
+
+    const container = containerRef.current;
+    const slides = container.querySelectorAll(".slide");
+    const containerWidth = slides.length * window.innerWidth;
+
+    gsap.set(container, { width: containerWidth });
+    gsap.set(slides, { width: window.innerWidth });
+
+    const horizontalScroll = gsap.to(container, {
+      x: () => -(containerWidth - window.innerWidth),
+      ease: "none",
+      scrollTrigger: {
+        trigger: container,
+        start: "top top",
+        end: () => "+=" + containerWidth,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    lenis.on("scroll", ScrollTrigger.update);
+
+    const onAnimationFrame = (time) => {
+      lenis.raf(time);
+      requestAnimationFrame(onAnimationFrame);
+    };
+
+    requestAnimationFrame(onAnimationFrame);
+
+    return () => {
+      lenis.destroy();
+      horizontalScroll.kill();
+    };
+  }, []);
+
+  return (
+    <div className="overflow-hidden h-screen w-screen">
+      <div ref={containerRef} className="flex h-screen">
+        <div className="slide">
+          Slide 1
+        </div>
+        <div className="slide">
+          Slide 2
+        </div>
+        <div className="slide">
+          Slide 3
+        </div>
+        <div className="slide">
+          Slide 4
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HorizontalScroll;
